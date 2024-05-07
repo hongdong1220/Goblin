fcc8c8c975492e846aaa673a9900aa4a071bf759
Hong Dong <59760823+hongdong1220@users.noreply.github.com>
2024-05-06 18:48:31 -0700

    rearranged files location

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("api_stock_price/<str:ticker>",
-         views.api_stock_price, name="api_stock_price"),
-]
